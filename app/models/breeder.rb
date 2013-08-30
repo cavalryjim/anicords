540e2799cebda4312d86785d933bd77eb1c02a68
James Davis <james.davisphd@gmail.com>
2013-08-30 12:37:42 -0500

    Nesting animals to the breeder model & view.  Still more work to be done.

--- app/models/breeder.rb
+++ app/models/breeder.rb
@@ -2,5 +2,7 @@ class Breeder < ActiveRecord::Base
   
   has_many  :user_associations, :dependent => :destroy
   has_many  :users, :through => :user_associations
+  has_many  :animals
+  accepts_nested_attributes_for :animals, allow_destroy: true
   
 end
