941b3b758f0c3417cb9d77b49d52ae4f3c2ddf23
James Davis <james.davisphd@gmail.com>
2013-10-22 07:02:01 -0500

    Continued with the file upload project for the animal module and added some additional icons where appropriate.

---
+++ app/assets/images/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @animal, :name, :type, :breed, :weight, :description, :created_at, :updated_at
--- app/helpers/animals_helper.rb
+++ app/helpers/animals_helper.rb
@@ -4,4 +4,11 @@ module AnimalsHelper
     pluralize(number, 'document') + ' uploaded'
   end
   
+  def pedigree_label(animal)
+    if animal.pedigree.file
+      html = label_tag('pedigree') + link_to(animal.pedigree.file.filename, animal.pedigree.url, target: '_blank')
+    end
+    
+  end
+  
 end
