4139bb582a5397a19e23abab99470b599bc814da
James Davis <james.davisphd@gmail.com>
2013-11-02 17:15:33 -0500

    annotated the medication table.

--- app/models/animal_medication.rb
+++ app/models/animal_medication.rb
@@ -1,6 +1,20 @@
+# == Schema Information
+#
+# Table name: animal_medications
+#
+#  id            :integer          not null, primary key
+#  animal_id     :integer
+#  medication_id :integer
+#  created_at    :datetime
+#  updated_at    :datetime
+#
+
 class AnimalMedication < ActiveRecord::Base
   belongs_to :animal
   belongs_to :medication
+  
+  validates_presence_of :animal_id
+  validates_presence_of :medication_id
   
   
   def name
--- app/models/medication.rb
+++ app/models/medication.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: medications
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Medication < ActiveRecord::Base
   has_many :animals, through: :animal_medications
   has_many :animal_medications
