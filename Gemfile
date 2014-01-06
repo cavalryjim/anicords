47f65297a8fa30ad6815f966816edda99856afec
James Davis <james.davisphd@gmail.com>
2014-01-05 20:26:56 -0600

    Changes made while writing the build instructions.

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
-ruby '2.0.0'
-#ruby-gemset=anicords
+#ruby=2.0.0-p247@anicords
+
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.2'
--- db/migrate/20131109194149_drop_animal_food.rb
+++ db/migrate/20131109194149_drop_animal_food.rb
@@ -1,6 +1,6 @@
 class DropAnimalFood < ActiveRecord::Migration
   def change
-    drop_table :animal_foods
+    drop_table :animal_foods if self.table_exists?("animal_foods")
     
     add_column :animals, :food_id, :integer
   end
