3687d7c41e10fa1be30e925e5a6470f5e3c4835d
James Davis <james.davisphd@gmail.com>
2014-05-29 19:28:21 -0500

    If no document type is select at upload, the system is automatically type it as 'other'.

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -27,7 +27,7 @@ class DocumentsController < ApplicationController
   # POST /documents.json
   def create
     @document = Document.new(document_params)
-    
+    @document.file_type = 'other' if @document.file_type.blank?
     #JDavis: if the params[:file_type] is blank, set it to 'other'.  JDHere.
     
     respond_to do |format|
