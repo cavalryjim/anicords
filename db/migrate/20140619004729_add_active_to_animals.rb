cf1520f6bf53ffbbb7c53813d9fe875763a2de57
cavalryjim <james.davisphd@gmail.com>
2014-06-19 16:09:21 -0500

    Update 20140619004729_add_active_to_animals.rb

--- db/migrate/20140619004729_add_active_to_animals.rb
+++ db/migrate/20140619004729_add_active_to_animals.rb
@@ -1,4 +1,8 @@
 class AddActiveToAnimals < ActiveRecord::Migration
+  #JDavis: need to rollback, add these changes, and migrate again.  JDhere.
+  # add_column :animals, :active, :boolean, default: true
+  # Animal.update_all(active: true)
+  
   def change
     add_column :animals, :active, :boolean
   end
