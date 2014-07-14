1c95849611c3ce3f6d7fe38e3bd1a8d7e48467bc
James Davis <james.davisphd@gmail.com>
2014-07-13 23:23:07 -0500

    Updating the association select page.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,15 +3,15 @@ module UsersHelper
   def image(association)
     case association.group_type
     when 'Household'
-      image_tag(s3_url('black-white-house-md.png'), size: '15x15')
+      image_tag(s3_url('black-white-house-md.png'), size: '50x50')
     when 'Breeder'
-      image_tag(s3_url('petabyt_icon.png'), size: '15x15')
+      image_tag(s3_url('petabyt_icon.png'), size: '50x50')
     when 'ServiceProvider'
-      image_tag(s3_url('provider_icon.png'), size: '15x15')
+      image_tag(s3_url('provider_icon.png'), size: '50x50')
     when 'Organization'
-      image_tag(s3_url('barn-icon.png'), size: '15x15')
+      image_tag(s3_url('barn-icon.png'), size: '50x50')
     else
-      image_tag(s3_url('petabyt_icon.png'), size: '15x15')
+      image_tag(s3_url('petabyt_icon.png'), size: '50x50')
     end
   end
 
