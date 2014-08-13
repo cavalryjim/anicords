30e068356d41c418092ad68d00b2918a324cffe0
James Davis <james.davisphd@gmail.com>
2014-08-13 00:50:23 -0500

    migration fix

--- db/migrate/20140812054959_rename_medication_type_to_medication_type.rb
+++ db/migrate/20140812054959_rename_medication_type_to_medication_type.rb
@@ -1,5 +1,9 @@
 class RenameMedicationTypeToMedicationType < ActiveRecord::Migration
   def change
-    rename_column(:medications, :type, :medication_type)
+    if self.table_exists?("type")
+      rename_column(:medications, :type, :medication_type) 
+    else
+      add_column :medications, :medication_type, :string
+    end
   end
 end
